03950eba2340d3ffdf13f70fd6be118f5284eb95
SpookySquidward <willrkramer@gmail.com>
2023-10-26 16:10:56 -0500

    Use dataset refactoring

--- test_install.py
+++ test_install.py
@@ -6,19 +6,20 @@ import csv
 
 
 # Open test.csv
-reader = dataset.data_reader()
+reader = dataset.csv_reader()
 csv_path = os.path.join(os.path.curdir, "dataset", "test.csv")
 reader.open_csv(csv_path)
 
 
 # Read a few lines of data
-print(reader.read(num_samples=5))
+print("\nReading data from a csv:")
+print(*reader.read(num_samples=5), sep='\n')
 
 
 # Shuffle the data and read a few more lines
 reader.shuffle()
-print()
-print(reader.read(num_samples=5))
+print("\nReading csv data after shuffling:")
+print(*reader.read(num_samples=5), sep='\n')
 
 
 # Read a full epoch of training data
@@ -40,7 +41,7 @@ star_counts = {
 for batch in reader.read_epoch(batch_size=100):
     for review in batch:
         # Get the label
-        stars = review[0]
+        stars = review.label
         # Store the label and total counts
         star_counts[stars] += 1
         star_counts["total"] += 1
@@ -48,32 +49,33 @@ for batch in reader.read_epoch(batch_size=100):
 print(f"\nTest dataset sample counts by star rating (label):\n{star_counts}")
 
 
-
 # Label remapping example
 reader.shuffle()
 
-# The csv module can give us a writer in just a few lines, so use that rather than implementing our
-# own custom writer
+# Initialize a csv writer to save our remapped labels
+writer = dataset.csv_writer()
 remapped_path = os.path.join(os.path.curdir, "dataset", "remapped_test.csv")
-with open(remapped_path, "w") as out_file:
-    writer = csv.writer(out_file, delimiter=dataset.data_reader.csv_delimiter, quotechar=dataset.data_reader.csv_quotechar, doublequote=dataset.data_reader.csv_doublequote, quoting=csv.QUOTE_ALL)
-    
-    # Read in all our data and get rid of all 3-star reviews; remap other reviews to either "pos"
-    # or "neg"
-    for batch in reader.read_epoch(batch_size=100):
+with open(remapped_path, mode="w", encoding=dataset.csv_encoding) as csv_file:
+    writer.use_csv(csv_file)
+
+    # Read all our input data and remap the labels
+    for batch in reader.read_epoch(batch_size=50):
         for review in batch:
-            stars = review[0]
+            stars = review.label
             if stars in ["1", "2"]:
-                writer.writerow(["neg"] + review[1:])
+                review.label = "negative"
             elif stars == "3":
                 continue
             elif stars in ["4", "5"]:
-                writer.writerow(["pos"] + review[1:])
+                review.label = "positive"
+            writer.write_review(review)
     
-    # Note: the csv module also has a 
+        # Note: we could also use the writer.write_reviews() method to write more than one review
+        # at a time:
+        # writer.write_reviews(batch)
 
 # Now we can read in the new data to make sure it was processed correctly
-reader_remapped = dataset.data_reader()
+reader_remapped = dataset.csv_reader()
 reader_remapped.open_csv(remapped_path)
-print()
-print(reader_remapped.read(num_samples=10))
+print("\nFirst 10 samples of remapped dataset:")
+print(*reader_remapped.read(num_samples=10), sep='\n')
