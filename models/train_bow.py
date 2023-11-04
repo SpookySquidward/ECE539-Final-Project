c74dbdaaa9f404709ad0a9b5697098fc89472000
frejaz <fhpe19@student.aau.dk>
2023-11-04 12:01:18 -0500

    Function for bow

--- models/train_bow.py
+++ models/train_bow.py
@@ -10,21 +10,46 @@ def train_bow(train_path: Path) -> LogisticRegression:
     :param train_path: path to a CSV file of reviews
     :return: a logistic regression model
     """
-    train_df = pd.read_csv(train_path)
 
-    # Get data
-    text_train = train_df[["title", "body"]]
-    y_train = train_df["sentiment"]
-
-    # The following code is primarily from https://www.kaggle.com/code/kashnitsky/topic-4-linear-models-part-4-pros-cons
-    # which was used as a reference in our proposal.
-    cv = CountVectorizer()
-    text_train = text_train["body"]
-    cv.fit(text_train)  # Figure out later how we handle titles
-    x_train = cv.transform(text_train)
+    x_train, y_train = format_text_to_bow(train_path)
 
     # Train a logistic regression on the transformed data
     log_reg = LogisticRegression(solver="lbfgs", n_jobs=-1, random_state=7)
     log_reg.fit(x_train, y_train)
 
     return log_reg
+
+
+def format_text_to_bow(path: Path) -> (pd.Series, pd.Series):
+    """
+    Function to format text to a bag of words (BoW).
+    The following code is primarily from https://www.kaggle.com/code/kashnitsky/topic-4-linear-models-part-4-pros-cons
+    :param path: path to CSV file to format
+    :return: tuple of x (words/feature) and y (label)
+    """
+    df = pd.read_csv(path)
+    text = df["body"]  # Figure out later how we handle titles
+    cv = CountVectorizer()
+    cv.fit(text)
+    x = cv.transform(text)
+    y = df["sentiment"]
+
+    return x, y
+
+
+def main():
+    # First parent is ...\ECE539-Final-Project\preprocessing, second is project root
+    project_root = Path(__file__).parent.parent
+    train_path = project_root.joinpath("dataset", "formatted_train.csv")
+    bow_model = train_bow(train_path)
+
+    # Get accuracy
+    test_path = project_root.joinpath("dataset", "formatted_test.csv")
+    x_train, y_train = format_text_to_bow(train_path)
+    x_test, y_test = format_text_to_bow(test_path)
+    print("Train accuracy: ", round(bow_model.score(x_train, y_train), 3))
+    print("Test Accuracy: ", round(bow_model.score(x_test, y_test), 3))
+
+
+if __name__ == "__main__":
+    main()
