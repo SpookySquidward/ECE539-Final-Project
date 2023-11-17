5b72bb639f16dead9e1d1610a0164c450eb365d6
Leah Gunn <lcgunn@wisc.edu>
2023-11-17 08:41:06 -0600

    Pushing what I have for now

--- model/bow_mlp.py
+++ model/bow_mlp.py
@@ -3,45 +3,23 @@ import re
 
 import pandas as pd
 import numpy as np
+import torch
 from pathlib import Path
 
-from bs4 import BeautifulSoup
 
-#from sklearn.feature_selection.univariate_selection import SelectKBest, chi2, f_classif
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import SGDClassifier, SGDRegressor
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import random
-import itertools
-
-import sys
-import os
-import argparse
-from sklearn.pipeline import Pipeline
-from scipy.sparse import csr_matrix
-from sklearn.feature_extraction.text import CountVectorizer
-import six
-from abc import ABCMeta
-from scipy import sparse
-from scipy.sparse import issparse
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils import check_X_y, check_array
-from sklearn.utils.extmath import safe_sparse_dot
-from sklearn.preprocessing import normalize, binarize, LabelBinarizer
-from sklearn.svm import LinearSVC
-
-import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import cm
 plt.style.use('ggplot')
 
-def mlp_data(train_path: Path):
-    train_data = pd.read_csv(filepath_or_buffer=train_path, names=["sentiment", "title", "body"])
-    
-    sns.countplot(train_data['sentiment'])
+def mlp_data(train_path: Path, test_path: Path):
+    train_dataframe = pd.read_csv(filepath_or_buffer=train_path, names=["sentiment", "title", "body"])
+    test_data = pd.read_csv(filepath_or_buffer=test_path, names=["sentiment", "title", "body"])
+
+    train_data = pd.DataFrame(train_dataframe["body"])
+    train_data=torch.tensor(train_data.values)
+
+    print(train_data)
+    #test_data=torch.tensor(test_data)
 
     return train_data
 
@@ -51,10 +29,10 @@ def main():
     # parent directory
     project_root = Path(__file__).parent.parent
     raw_train_path = project_root.joinpath("dataset", "1formatted_train.csv")
-    raw_test_path = project_root.joinpath("dataset", "1formatted_test.csv")
+    raw_test_path = project_root.joinpath("dataset", "1formatted_val.csv")
 
     # Reformat data
-    mlp_train = mlp_data(raw_train_path)
+    mlp_train = mlp_data(raw_train_path, raw_test_path)
 
     # Write to csv
     #mlp_train.to_csv(project_root.joinpath("dataset", "mlp_train.csv"), index=False)
