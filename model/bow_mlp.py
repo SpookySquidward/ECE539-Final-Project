4dd13016d9e2969f8b83c5cdf37c418be4e09161
Leah Gunn <lcgunn@wisc.edu>
2023-11-29 10:47:46 -0600

    I'm testing

--- model/bow_mlp.py
+++ model/bow_mlp.py
@@ -12,11 +12,13 @@ from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import mlp_helper
+import csv
 plt.style.use('ggplot')
 
 
 def converter(train_path: Path, test_path: Path, num_epochs):
     """Converts from a dataframe to tensor"""
+    project_root = Path(__file__).parent.parent
     train_dataframe = pd.read_csv(train_path)
     test_dataframe = pd.read_csv(test_path)
     
@@ -27,15 +29,20 @@ def converter(train_path: Path, test_path: Path, num_epochs):
 
     for epoch in range(num_epochs):
         train_1hot = mlp_helper.to1hot(train_dataframe[:split])
-        print(train_1hot)
-        #train_data = torch.tensor(train_1hot['sentiment'])
+        if epoch == 0:
+            train_data = torch.Tensor(train_1hot.values)
+        else:
+            train_data = torch.cat((train_data, torch.tensor(train_1hot.values)), 1)
         train_dataframe = train_dataframe[split:]
 
-        #test_1hot = mlp_helper.to1hot(test_dataframe[:split_test])
-        #test_data = torch.tensor(test_1hot)
-        #test_dataframe = test_dataframe[split_test:]
+        test_1hot = mlp_helper.to1hot(test_dataframe[:split_test])
+        if epoch == 0:
+            test_data = torch.Tensor(test_1hot.values)
+        else:
+            test_data = torch.cat((test_data, torch.tensor(test_1hot.values)), 1)
+        test_dataframe = test_dataframe[split_test:]
     
-    return 0
+    return train_data, test_data
 
 def mlp(train_data, test_data, lr, num_epochs, batch_size):
     """Trains the data through an mlp"""
@@ -91,16 +98,11 @@ def main():
     lr = 0.01
     batch_size = 16
 
-    mlp_data = converter(raw_train_path, raw_test_path,num_epochs)
+    train_data, test_data = converter(raw_train_path, raw_test_path,num_epochs)
 
+    print("Training Data Shape: ", train_data.shape)
 
-    # Write to csv
-    #mlp_train.to_csv(project_root.joinpath("dataset", "mlp_train.csv"), index=False)
-
-    # Check sizes
-    print("--> Running mlp_data")
-    rows, columns = mlp_data.shape
-    print("\nDataframe shape for train\nrows: ", rows)
+    #mlp(train_data, test_data, lr, num_epochs, batch_size)
 
 
 
